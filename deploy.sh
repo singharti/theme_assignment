660e58d0ce40b26704a6c0c024c38e8a7b145dda
singharti <artisingh3106@gmail.com>
2019-07-05 19:19:59 +0530

    theme

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+sudo php bin/magento setup:upgrade
+
+sudo php bin/magento setup:static-content:deploy -f
+
+sudo php bin/magento setup:di:compile
+
+sudo php bin/magento c:c
+
+sudo php bin/magento c:f
+
+sudo chmod -R 777 var/ pub/ generated/ app/
